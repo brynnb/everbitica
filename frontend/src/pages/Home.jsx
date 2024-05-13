c5d32f3525020392b243d83491c5267baf97bbfb
brynnb <brynnb@users.noreply.github.com>
2024-05-13 17:33:40 -0500

    Getting more React structure in place

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import Note from "../components/Note"
+import "../styles/Home.css"
+
+function Home() {
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+
+}
+
+export default Home;
